dfe00c836671a08cc5e3d0421262569c13232684
Arnab048 <codingjedi048@gmail.com>
2024-01-06 07:53:59 +0600

    Day-2

---
+++ client/src/components/Layout/Header.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../../index.css";
+const Header = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <NavLink className="navbar-brand" href="#">
+              Hidden brand
+            </NavLink>
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink to="/" className="nav-link">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/category" className="nav-link">
+                  Category
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/register" className="nav-link">
+                  Register
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/login" className="nav-link">
+                  Log In
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/cart" className="nav-link" href="#">
+                  Cart (0)
+                </NavLink>
+              </li>
+            </ul>
+            {/* <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form> */}
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
